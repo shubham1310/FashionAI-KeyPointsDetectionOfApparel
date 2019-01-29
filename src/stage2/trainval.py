0d0ad87ca133955a8212218634719ba7a460734b
Shubham Jain <shubham9@illinois.edu>
2019-01-28 21:14:00 -0500

    changed logger

--- src/stage2/trainval.py
+++ src/stage2/trainval.py
@@ -21,7 +21,7 @@ from src.stage2.data_generator import DataGenerator
 from src.stage2.cascade_pyramid_network import CascadePyramidNet
 from src.stage2v9.cascade_pyramid_network_v9 import CascadePyramidNetV9
 from src.stage2.viserrloss import VisErrorLoss
-from src.lr_scheduler import LRScheduler
+# from src.lr_scheduler import LRScheduler
 
 
 # def print_log(epoch, lr, train_metrics, train_time, val_metrics=None, val_time=None, save_dir=None, log_mode=None):
@@ -62,7 +62,7 @@ def train(data_loader, net, loss, optimizer, lr, epoch):
         loss_output[0].backward()
         optimizer.step()
         metrics.append([loss_output[0].item(), loss_output[1].item(), loss_output[2].item()])
-        if (i%5==0):
+        if (i%50==0):
             print("%d/%d loss:%.3f"%(i,len(data_loader),loss_output[0].data))
             log_value('iteration loss', loss_output[0].data, i+epoch*len(data_loader))
     end_time = time.time()
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     parser.add_argument('--mu', type=float, default=0.65, help='mu')
     parser.add_argument('--sigma', type=float, default=0.25, help='sigma ')
     parser.add_argument('--gpus', type=str, default='0', help='gpus')
-    parser.add_argument('--out', type=str, default='checkpoints/', help='output directory')
+    parser.add_argument('--out', type=str, default='stage2/', help='output directory')
     parser.add_argument('--train', type=int, default=0, help='train 0/val 1')
 
 
@@ -116,11 +116,11 @@ if __name__ == '__main__':
     # 256 pixels: SGD L1 loss starts from 1e-2, L2 loss starts from 1e-3
     # 512 pixels: SGD L1 loss starts from 1e-3, L2 loss starts from 1e-4
     base_lr = args.lr
-    save_dir = args.out
+    save_dir = 'log/' + args.out
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
 
-    configure(args.out, flush_secs=5)
+    configure('log/' + args.out, flush_secs=5)
 
     net = CascadePyramidNet(config)
     loss = VisErrorLoss()
@@ -132,14 +132,14 @@ if __name__ == '__main__':
     start_epoch = 1
     lr = base_lr
     best_val_loss = float('inf')
-    log_mode = 'w'
+    # log_mode = 'w'
     if args.resume is not None:
         checkpoint = torch.load(args.resume)
-        start_epoch = checkpoint['epoch'] + 1
-        lr = checkpoint['lr']
-        best_val_loss = checkpoint['best_val_loss']
+        # start_epoch = checkpoint['epoch'] + 1
+        # lr = checkpoint['lr']
+        # best_val_loss = checkpoint['best_val_loss']
         net.load_state_dict(checkpoint['state_dict'])
-        log_mode = 'a'
+        # log_mode = 'a'
 
     net = net.cuda()
     loss = loss.cuda()
@@ -164,31 +164,39 @@ if __name__ == '__main__':
 
     if args.train==0:
 
-        lrs = LRScheduler(lr, patience=3, factor=0.1, min_lr=0.01*lr, best_loss=best_val_loss)
+        # lrs = LRScheduler(lr, patience=3, factor=0.1, min_lr=0.01*lr, best_loss=best_val_loss)
         for epoch in range(start_epoch, epochs + 1):
             train_metrics, train_time = train(train_loader, net, loss, optimizer, lr, epoch)
             with torch.no_grad():
                 val_metrics, val_time = validate(val_loader, net, loss)
 
             # print_log(epoch, lr, train_metrics, train_time, val_metrics, val_time, save_dir=save_dir, log_mode=log_mode)
-
-            log_value('train loss',  train_metrics[0], epoch)
-            log_value('train loss 1',  train_metrics[1], epoch)
-            log_value('train loss 2',  train_metrics[2], epoch)
+            train_metrics = np.array(train_metrics)
+            n= len(train_metrics)
+            log_value('train loss',  sum(train_metrics[:,0])/n, epoch)
+            log_value('train loss 1',  sum(train_metrics[:,0])/n, epoch)
+            log_value('train loss 2',  sum(train_metrics[:,0])/n, epoch)
             log_value('train time',  train_time, epoch)
-            log_value('validation loss',  val_metrics[0], epoch)
-            log_value('validation loss 1',  val_metrics[1], epoch)
-            log_value('validation loss 2',  val_metrics[2], epoch)
+            val_metrics = np.array(val_metrics)
+            n= len(val_metrics)
+            log_value('validation loss',  sum(val_metrics[:,0])/n, epoch)
+            log_value('validation loss 1',  sum(val_metrics[:,1])/n, epoch)
+            log_value('validation loss 2',  sum(val_metrics[:,2])/n, epoch)
             log_value('validation time',  val_time, epoch)
 
             val_loss = np.mean(val_metrics[:, 0])
-            lr = lrs.update_by_rule(val_loss)
-            if val_loss < best_val_loss or epoch%10 == 0 or lr is None:
-                if val_loss < best_val_loss:
-                    best_val_loss = val_loss
+            # lr = lrs.update_by_rule(val_loss)
+            if val_loss < best_val_loss or epoch%4 == 0 or lr is None:
                 state_dict = net.module.state_dict()
                 for key in state_dict.keys():
                     state_dict[key] = state_dict[key].cpu()
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    log_value('best validation loss',  best_val_loss, epoch)
+                    torch.save({
+                        'state_dict': state_dict,
+                        'best_val_loss': best_val_loss},
+                        os.path.join(save_dir, 'kpt_'+config.clothes+'_best_%03d.ckpt' % epoch))
                 torch.save({
                     'epoch': epoch,
                     'save_dir': save_dir,
@@ -198,14 +206,17 @@ if __name__ == '__main__':
                     os.path.join(save_dir, 'kpt_'+config.clothes+'_%03d.ckpt' % epoch))
             if lr is None:
                 print('Training is early-stopped')
+                # print('best_val_loss %f'%(best_val_loss))
                 break
 
 
     else:
         with torch.no_grad():
             val_metrics, val_time = validate(val_loader, net, loss)
+        val_metrics = np.array(val_metrics)
+        n= len(val_metrics)
         print('Validation: time %3.2f loss: %2.4f loss1: %2.4f loss2: %2.4f \n' \
-               % (val_time, val_metrics[0], val_metrics[1], val_metrics[2]))
+               % (val_time, sum(val_metrics[:,0])/n, sum(val_metrics[:,1])/n, sum(val_metrics[:,2])/n))
 
 
 
